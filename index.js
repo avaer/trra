b507e51873f34023fb6cacb33514645d9aa210f8
Adrian Biedrzycki <a@modules.io>
2017-08-29 03:04:24 -0700

    Return removed chunk from removeChunk method

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ class Trra {
   }
 
   removeChunk(x, z) {
-    this.chunks.splice(this.chunks.findIndex(chunk => chunk.x === x && chunk.z === z), 1);
+    return this.chunks.splice(this.chunks.findIndex(chunk => chunk.x === x && chunk.z === z), 1)[0];
   }
 
   makeChunk(x, z) {
